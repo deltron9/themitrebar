24f38b05df1530b4f9f2412f916c83ea0bfa48d2
deltron9 <cristian_collante@hotmail.com>
2026-04-14 10:54:31 -0300

    delete: require repetido

--- server/routes.js
+++ server/routes.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
-const express = require('express');
 const jwt = require('jsonwebtoken');
 const multer = require('multer');
 
