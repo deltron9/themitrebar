64655db65368aab2a9da39a115dbb23730dcbbb8
deltron9 <cristian_collante@hotmail.com>
2026-04-17 15:09:01 -0300

    add: logica de libreria swiper para carrusel de fotos

--- public/js/script.js
+++ public/js/script.js
@@ -1,25 +1,100 @@
-const sections = document.querySelectorAll("main[id], section[id]");
-const navLinks = document.querySelectorAll(".nav-links a");
+document.addEventListener('DOMContentLoaded', () => {
 
-/*LOGICA DE SCROLL*/
-window.addEventListener("scroll", () => {
-    let currentSection = "";
+    const sections = document.querySelectorAll("main[id], section[id]");
+    const navLinks = document.querySelectorAll(".nav-links a");
 
-    sections.forEach((section) => {
-        const sectionTop = section.offsetTop;
-        const sectionHeight = section.clientHeight;
-        
-        if (window.pageYOffset >= sectionTop - 150) {
-            currentSection = section.getAttribute("id");
-        }
+    /*LOGICA DE SCROLL*/
+    window.addEventListener("scroll", () => {
+        let currentSection = "";
+
+        sections.forEach((section) => {
+            const sectionTop = section.offsetTop;
+            const sectionHeight = section.clientHeight;
+            
+            if (window.pageYOffset >= sectionTop - 150) {
+                currentSection = section.getAttribute("id");
+            }
+        });
+
+        navLinks.forEach((link) => {
+            link.classList.remove("active");
+            if (link.getAttribute("href") === `#${currentSection}`) {
+                link.classList.add("active");
+            }
+        });
     });
 
-    navLinks.forEach((link) => {
-        link.classList.remove("active");
-        if (link.getAttribute("href") === `#${currentSection}`) {
-            link.classList.add("active");
+    /* LOGICA DEL BOTON DE PEDIDOS */
+    const orderBtn = document.getElementById('orderBtn');
+    const deliveryOptions = document.getElementById('deliveryOptions');
+
+    if (orderBtn && deliveryOptions) {
+        orderBtn.addEventListener('click', (e) => {
+            e.stopPropagation();
+            deliveryOptions.classList.toggle('show');
+        });
+
+        document.addEventListener('click', () => {
+            deliveryOptions.classList.remove('show');
+        });
+    }
+
+    /*logica para cierre de modal con pdf al hacer click fuera*/
+    window.onclick = function(e) {
+        const modal = document.getElementById('pdfModal');
+        if (e.target === modal) {
+            closePdfModal();
         }
-    });
+    };
+
+    const logo = document.querySelector('.logo-area');
+    
+    if (logo) {
+        const currentPage = logo.getAttribute('data-page');
+
+        if (currentPage === 'inicio') {
+            let clickCount = 0;
+            let clickTimer;
+
+            logo.addEventListener('click', () => {
+                clickCount++;
+
+                clearTimeout(clickTimer);
+                clickTimer = setTimeout(() => {
+                    clickCount = 0;
+                }, 2000);
+
+                if (clickCount === 5) {
+                    window.location.href = '/login';
+                }
+            });
+        }
+    }
+
+    //logica para carrusel de libreria swiper
+    const configFade = {
+        effect: 'fade', //animacion de desvanecimiento
+        fadeEffect: {
+            crossFade: true //cruza las opacidades de ambas fotos
+        },
+        loop: true,
+        speed: 5000, // Duración de la transición (2 segundo)
+        autoplay: {
+            delay: 4000, //tiempo que se queda fija la foto (3csegundos)
+            disableOnInteraction: false,
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+    };
+
+    if (document.querySelector('.swiper-esencia')) {
+        new Swiper('.swiper-esencia', configFade);
+    }
+    if (document.querySelector('.swiper-carta')) {
+        new Swiper('.swiper-carta', configFade);
+    }
 });
 
 /*LOGICA DEL MODAL PDF*/
@@ -45,52 +120,3 @@ function closePdfModal() {
         document.body.style.overflow = 'auto'; //devuelve el scroll
     }
 }
-
-/* LOGICA DEL BOTON DE PEDIDOS */
-const orderBtn = document.getElementById('orderBtn');
-const deliveryOptions = document.getElementById('deliveryOptions');
-
-if (orderBtn && deliveryOptions) {
-    orderBtn.addEventListener('click', (e) => {
-        e.stopPropagation();
-        deliveryOptions.classList.toggle('show');
-    });
-
-    document.addEventListener('click', () => {
-        deliveryOptions.classList.remove('show');
-    });
-}
-
-/*logica para cierre de modal con pdf al hacer click fuera*/
-window.onclick = function(e) {
-    const modal = document.getElementById('pdfModal');
-    if (e.target === modal) {
-        closePdfModal();
-    }
-};
-
-document.addEventListener('DOMContentLoaded', () => {
-    const logo = document.querySelector('.logo-area');
-    
-    if (logo) {
-        const currentPage = logo.getAttribute('data-page');
-
-        if (currentPage === 'inicio') {
-            let clickCount = 0;
-            let clickTimer;
-
-            logo.addEventListener('click', () => {
-                clickCount++;
-
-                clearTimeout(clickTimer);
-                clickTimer = setTimeout(() => {
-                    clickCount = 0;
-                }, 2000);
-
-                if (clickCount === 5) {
-                    window.location.href = '/login';
-                }
-            });
-        }
-    }
-});
